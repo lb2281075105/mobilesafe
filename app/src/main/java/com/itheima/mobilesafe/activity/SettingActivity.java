d918bf1f3487dbbd0f1370d25996bfdfef94c4a3
liubo <2281075105@qq.com>
2020-09-15 15:05:41 +0800

    单选框

--- app/src/main/java/com/itheima/mobilesafe/activity/SettingActivity.java
+++ app/src/main/java/com/itheima/mobilesafe/activity/SettingActivity.java
@@ -1,11 +1,12 @@
 package com.itheima.mobilesafe.activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
-import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.itheima.mobilesafe.R;
@@ -17,7 +18,10 @@ import com.itheima.mobilesafe.view.SettingClickView;
 import com.itheima.mobilesafe.view.SettingItemView;
 
 public class SettingActivity extends AppCompatActivity {
+    private int toast_style;
     private String[] mToastStyle;
+    private SettingClickView settingClickView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,43 +37,78 @@ public class SettingActivity extends AppCompatActivity {
         // 吐司样式
         initToast();
     }
-    public void initToast(){
 
-        SettingClickView settingClickView = findViewById(R.id.scv_toast_style);
+    public void initToast() {
+
+
+        // 1.
+        settingClickView = findViewById(R.id.scv_toast_style);
         settingClickView.setTitle("设置归属地显示风格");
 
-        mToastStyle = new String[]{"透明","橙色","蓝色","灰色","绿色"};
-        int toast_style = SpUtil.getInt(getApplicationContext(),ConstantValue.TOAST_STYLE,0);
+        mToastStyle = new String[]{"透明", "橙色", "蓝色", "灰色", "绿色"};
+        toast_style = SpUtil.getInt(getApplicationContext(), ConstantValue.TOAST_STYLE, 0);
         settingClickView.setDes(mToastStyle[toast_style]);
+        settingClickView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showToastStyle();
+            }
+        });
 
 
-
+        // 2.
         SettingClickView addressClickView = findViewById(R.id.address_toast_style);
         addressClickView.setTitle("归属地提示框位置");
         addressClickView.setDes("设置归属地提示框位置");
 
+        // 3.
         final SettingItemView heiView = findViewById(R.id.hei_address);
         heiView.setCheck(true);
 
 
+        // 4.
         final SettingItemView chengView = findViewById(R.id.cheng_address);
         chengView.setCheck(true);
     }
+
+    public void showToastStyle() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setIcon(R.drawable.lock);
+        builder.setTitle("请选择归属地样式");
+        builder.setSingleChoiceItems(mToastStyle, toast_style, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                toast_style = i;
+                Log.e("HHH", i + "");
+                SpUtil.putInt(getApplicationContext(), ConstantValue.TOAST_STYLE, i);
+                dialogInterface.dismiss();
+                settingClickView.setDes(mToastStyle[i]);
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+    }
+
     // 电话归属地设置
-    public void initAddress(){
+    public void initAddress() {
 
         final SettingItemView itemView = findViewById(R.id.siv_address);
-        boolean isRunning = ServiceUtil.isRunning(getApplicationContext(),"com.itheima.mobilesafe.service.AddressService");
+        boolean isRunning = ServiceUtil.isRunning(getApplicationContext(), "com.itheima.mobilesafe.service.AddressService");
         itemView.setCheck(isRunning);
         itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 boolean ischeck = itemView.isCheck();
                 itemView.setCheck(!ischeck);
-                Log.i("SettingActivity","ischeck"+!ischeck);
-                if (!ischeck){
+                Log.i("SettingActivity", "ischeck" + !ischeck);
+                if (!ischeck) {
                     startService(new Intent(getApplicationContext(), AddressService.class));
-                }else {
+                } else {
                     stopService(new Intent(getApplicationContext(), AddressService.class));
                 }
             }
@@ -77,16 +116,16 @@ public class SettingActivity extends AppCompatActivity {
     }
 
     // 自动更新设置
-    public void initUpdate(){
+    public void initUpdate() {
         final SettingItemView itemView = findViewById(R.id.setting_item_view);
-        boolean open_update = SpUtil.getBoolean(this, ConstantValue.OPEN_UPDATE,false);
+        boolean open_update = SpUtil.getBoolean(this, ConstantValue.OPEN_UPDATE, false);
         itemView.setCheck(open_update);
         itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 boolean ischeck = itemView.isCheck();
                 itemView.setCheck(!ischeck);
-                SpUtil.putBoolean(getApplicationContext(),ConstantValue.OPEN_UPDATE,!ischeck);
+                SpUtil.putBoolean(getApplicationContext(), ConstantValue.OPEN_UPDATE, !ischeck);
             }
         });
     }
