e0ed076a95f2bdc4b5fb8b8bb4833abb90c61eb1
liubo <2281075105@qq.com>
2020-09-21 16:19:16 +0800

    短信备份

--- app/src/main/java/com/itheima/mobilesafe/activity/AToolActivity.java
+++ app/src/main/java/com/itheima/mobilesafe/activity/AToolActivity.java
@@ -1,7 +1,12 @@
 package com.itheima.mobilesafe.activity;
 
+import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -9,15 +14,22 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.itheima.mobilesafe.R;
+import com.itheima.mobilesafe.engine.SmsBackUp;
+
+import java.io.File;
 
 public class AToolActivity extends AppCompatActivity {
     private TextView phone_address;
+    private TextView tv_sms_backup;
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_atool);
 
         initUI();
+
     }
     public void initUI(){
 
@@ -28,5 +40,52 @@ public class AToolActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(),QueryAddressActivity.class));
             }
         });
+
+        tv_sms_backup = findViewById(R.id.tv_sms_backup);
+        tv_sms_backup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                    int hasReadSmsPermission = checkSelfPermission(Manifest.permission.READ_SMS);
+                    if (hasReadSmsPermission != PackageManager.PERMISSION_GRANTED) {
+                        requestPermissions(new String[]{Manifest.permission.READ_SMS}, REQUEST_CODE_ASK_PERMISSIONS);
+                        return;
+                    }
+                }
+                showSmsBackupDialog();
+            }
+        });
+    }
+
+    private void showSmsBackupDialog() {
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setIcon(R.drawable.home_apps);
+        progressDialog.setTitle("短信备份");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progressDialog.show();
+
+        new Thread(){
+            @Override
+            public void run() {
+
+
+//                String path = Environment.getExternalStorageDirectory().getPath() + File.separator + "sms.xml";
+                String path = "/data/data/com.itheima.mobilesafe/files/" + "sms.xml";
+
+                SmsBackUp.backup(getApplicationContext(), path, new SmsBackUp.CallBack() {
+                    @Override
+                    public void setMax(int max) {
+                        Log.e("HHH",max+"");
+                        progressDialog.setMax(max);
+                    }
+
+                    @Override
+                    public void setProgress(int index) {
+                        progressDialog.setProgress(index);
+
+                    }
+                });
+            }
+        }.start();
     }
 }
